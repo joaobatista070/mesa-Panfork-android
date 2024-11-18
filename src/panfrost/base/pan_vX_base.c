79ab7ca20e2f4381a0fb52edc49e71ab795b0c45
joaobatista070 <joaobat0102@gmail.com>
2024-11-18 17:34:05 -0300

    Update pan_vX_base.c

--- src/panfrost/base/pan_vX_base.c
+++ src/panfrost/base/pan_vX_base.c
@@ -507,8 +507,7 @@ static struct kbase_op kbase_main[] = {
         { init_mem_jit, NULL, "Initialise JIT allocator" },
 #endif
 #if PAN_BASE_API >= 1
-        { init_mem_exec, NULL, "Initialise EXEC_VA zone" },
-        { init_mem_jit, NULL, "Initialise JIT allocator" },
+        { init_mem_exec, NULL, "Initialise EXEC_VA zone" },   
 #endif
 #if PAN_BASE_API >= 2
         { alloc_event_mem, free_event_mem, "Allocate event memory" },
